2dba5ad51c5a80275de6cfa97a96da46b56f408e
Tarek-Sb <tksb06712@gmail.com>
2024-08-29 07:09:05 +0300

    fixing a layer mistake

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/navbar";
 export default function App() {
   return (
       <main dir="rtl" className="flex justify-start items-start flex-col gap-[50px]">
-        <nav className="w-[100vw] h-[75px]">
+        <nav className="w-[100vw] h-[75px] z-10">
           <Navbar></Navbar>
         </nav>
         <section className="pt-[0px]">
