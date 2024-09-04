96f50e7eaadfa50db0300f2cbb7287076440e364
Tarek-Sb <tksb06712@gmail.com>
2024-09-04 20:04:37 +0300

    testing Rubik font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["arabic"] })
 
 export const metadata = {
   title: "TelePort",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar">
-      <body className={`${inter.className} bg-[#FBFBFB] h-[2000px] font-sans`}>
+      <body className={`${rubik.className} bg-[#FBFBFB] h-[2000px]  `}>
         {children}
       </body>
     </html>
