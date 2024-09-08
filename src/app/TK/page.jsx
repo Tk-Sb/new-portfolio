f86339c611d60caee241ff16d78699eb40220d61
Tarek-Sb <tksb06712@gmail.com>
2024-09-08 18:19:01 +0300

    testing hover cards

--- src/app/TK/page.jsx
+++ src/app/TK/page.jsx
@@ -1,6 +1,3 @@
-import FeatureCards from "@/components/feature-cards/feature-cards";
-import Dock from "@/components/dock/dock";
-import AnimatedBeam from "@/components/animated-beam/animated-beam";
 import HoverCard from "@/components/hover-card/hover-card";
 
 export default function page() {
